4249a12a757c5c1bf0bb058849757dbef20b3fac
daveit86 <daveit10@gmail.com>
2022-06-18 21:32:37 +0200

    Adds logic to create the pixels

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("loaded!");
+
+const screen=document.querySelector("#screen");
+const btn=document.querySelector("button");
+
+function populateScreen(sideNum)
+{
+    for(let i=0;i<sideNum;i++)
+    {
+        let pixel=document.createElement("div");
+        pixel.classList.add("pixel");
+        screen.appendChild(pixel);
+    }
+}
+
+function promptSideNum()
+{
+    return prompt("Number of pixels per side?");
+}
+
+btn.addEventListener("mouseup",(e)=>{
+    let sideNum=promptSideNum();
+    populateScreen(sideNum);
+})
